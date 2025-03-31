8894bf2a941462690c8e696880269f8eba5be339
cjsanchez17 <carlos17@mit.edu>
2025-03-31 20:42:24 +0000

    motion model done

--- localization/motion_model.py
+++ localization/motion_model.py
@@ -37,22 +37,20 @@ class MotionModel:
         ####################################
         # TODO
 
-        # x_k-1 = particles[-1]
-        # del_x = odom
-
-        for k1 in particles:
-            del_pos = np.array([odometry[:2]]).T
-            theta_w_r = odometry[2]
-
-            # x_k1 = particles[-1]
-            x_k1 = particles[k1]
-            theta_t1 = x_k1[2, 0]
+        
+        new_particles = np.empty(particles.shape)
+        del_pos = np.array([odometry[:2]]).T
+        theta_w_r = odometry[2]
+        for i in range(len(particles)):
+            pose = particles[i]
+            x_k1 = np.array([pose[:2]]).T
+            theta_t1 = pose[-1]
             R_w_r = np.array([[cos(theta_t1), -sin(theta_t1)], [sin(theta_t1), cos(theta_t1)]])
-            pos_new = R_w_r @ del_pos + x_k1[:2]
-            x_update = np.vstack((pos_new, x_k1[2] + odometry[2]))
+            x_k = R_w_r @ del_pos + x_k1
+            x_update = np.vstack((x_k, theta_t1 +theta_w_r)).T
+            new_particles[i]=x_update
 
-            particles[k1] = x_update #.T[0]
-
+        return new_particles
         ####################################
 
 
