Fixed normalization issue

localization/sensor_model.py
@@ -39,7 +39,7 @@ class SensorModel:
         self.alpha_max = 0.07
         self.alpha_rand = 0.12
         self.sigma_hit = 8.0
-        self.z_max = 200
+        self.z_max = 201
 
         # Your sensor table will be a `table_width` x `table_width` np array:
         self.table_width = 201
@@ -118,22 +118,22 @@ class SensorModel:
 
         # rows = i = z values
         # cols = j = d values
+        p_hits = np.zeros((len(self.sensor_model_table), len(self.sensor_model_table[0])))
         for i in range(len(self.sensor_model_table)):
-            p_hits = []
-            p_hit_sum = 0.0 # used to notmalize the p_hit
-            for j in range(len(self.sensor_model_table[0])):
-                p_hit = self.p_hit(i,j)
-                p_hit_sum += p_hit
-                p_hits.append(p_hit)
-            
-            norm_alpha_hit = self.alpha_hit/p_hit_sum
             
             for j in range(len(self.sensor_model_table[0])):
-                p_update = norm_alpha_hit * p_hits[j] + self.alpha_short * self.p_short(i, j) + self.alpha_max * self.p_max(i, j) + self.alpha_rand * self.p_rand(i, j)
+                p_update = self.alpha_short * self.p_short(i, j) + self.alpha_max * self.p_max(i, j) + self.alpha_rand * self.p_rand(i, j)
                 self.sensor_model_table[i][j] = p_update
+                p_hits[i,j] = self.p_hit(i,j)
+
+        norms = np.sum(p_hits, axis=0, keepdims = True)
+        p_hits /= norms
+        p_hits *= self.alpha_hit
+
+        self.sensor_model_table += p_hits
 
         
-        self.sensor_model_table/self.sensor_model_table.sum(axis=0,keepdims=1)
+        self.sensor_model_table/=np.sum(self.sensor_model_table, axis=0,keepdims=1)
 
     def evaluate(self, particles, observation):
         """